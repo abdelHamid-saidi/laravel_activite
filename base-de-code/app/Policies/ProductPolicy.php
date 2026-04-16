1615cdb98755605c8a2e727f2fc20038faaa8691
Abdelhamid Saidi <saidiabdelhamid.office@gmail.com>
2026-04-16 12:57:34 +0200

    ACTIVITE 8

--- base-de-code/app/Policies/ProductPolicy.php
+++ base-de-code/app/Policies/ProductPolicy.php
@@ -8,6 +8,13 @@ use Illuminate\Auth\Access\Response;
 
 class ProductPolicy
 {
+    public function before(User $user)
+    {
+        if ($user->hasRole('admin')) {
+            return true;
+        }
+    }
+
     /**
      * Determine whether the user can view any models.
      */
--- base-de-code/database/seeders/DatabaseSeeder.php
+++ base-de-code/database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,12 +18,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // créer rôle
+        $adminRole = Role::create(['name' => 'admin']);
+
+        // créer permission
+        $permission = Permission::create(['name' => 'view dashboard']);
+
+        // assigner permission au rôle
+        $adminRole->givePermissionTo($permission);
+
         // Utilisateur "admin"
         $admin = User::factory()->create([
             'name'     => 'Admin',
             'email'    => 'admin@example.com',
             'password' => bcrypt('password'),
         ]);
+
+        // assigner rôle
+        $admin->assignRole($adminRole);
 
         // Utilisateur "user"
         $user = User::factory()->create([
